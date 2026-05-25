46564da503675145391fff4005a21c47063d3dac
Melkam Worku <melkam2490@gmail.com>
2026-05-24 19:01:57 -0700

    Added Review and Tag models

--- projects/models.py
+++ projects/models.py
@@ -4,14 +4,45 @@ import uuid
 # Create your models here.
 
 
-class project(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True)
+    vote_ratio = models.IntegerField(default=0, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    # owner=
+    project = models.ForeignKey(
+        'Project', on_delete=models.SET_NULL, null=True)
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('Down', 'Down Vote',
+         ))
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=250, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
